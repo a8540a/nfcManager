715777eb90fd19fae8dcc3187bef8ee9d272bc4f
a8540a <a8540a@kyonggi.ac.kr>
2022-05-01 21:10:13 +0900

    nfc에 읽기쓰기위한 데이처 처리 기능

--- app/src/main/java/com/example/nfcmanager/dataController.java
+++ app/src/main/java/com/example/nfcmanager/dataController.java
@@ -1,5 +1,57 @@
 package com.example.nfcmanager;
 
+
+
+
+/*
+tag입력을 위한 데이터 컨트롤
+tag는 4byte로 구성된 block단위로 write지원
+따라서 데이터를 byte형태로 변환하고 데이터간 구분을 위해 문자열 "\"(백슬래시) 사용 (입력값에는 백슬래시가 존재하면 안됨)
+문자열 처리는 UTF-8이용
+ */
 public class dataController {
-    
+
+    //String을 byte배열로 변경해주고 구분을 위한 "\"(백슬래시) 추가
+    public byte[] stringToByte(String a){
+        String c = a+"\\";
+
+        return c.getBytes();
+    }
+
+    public String byteToString(byte[] a){
+        String string = new String(a);
+
+        return string.replace("\\","");
+    }
+
+    public String byteToString2(byte[] a){
+        String string = new String(a);
+        return string;
+    }
+
+    public byte[] mergeStringToByte(String a,String b,String c,String d,String e){
+        byte[] abyte = stringToByte(a);
+        byte[] bbyte = stringToByte(b);
+        byte[] cbyte = stringToByte(c);
+        byte[] dbyte = stringToByte(d);
+        byte[] ebyte = stringToByte(e);
+
+        int totalLength = abyte.length+bbyte.length+cbyte.length+dbyte.length+ebyte.length;
+        byte[] newbyte = new byte[totalLength];
+
+        System.arraycopy(abyte,0,newbyte,0,abyte.length);
+        System.arraycopy(bbyte,0,newbyte,abyte.length,bbyte.length);
+        System.arraycopy(cbyte,0,newbyte,abyte.length+bbyte.length,cbyte.length);
+        System.arraycopy(dbyte,0,newbyte,abyte.length+bbyte.length+cbyte.length,dbyte.length);
+        System.arraycopy(ebyte,0,newbyte,abyte.length+bbyte.length+cbyte.length+dbyte.length,ebyte.length);
+
+        return newbyte;
+    }
+    public String[] mergeByteToString(byte[] a){
+        String[] array = byteToString2(a).split("\\\\");
+        return array;
+    }
+
+
+
 }
--- app/src/test/java/com/example/nfcmanager/ExampleUnitTest.java
+++ app/src/test/java/com/example/nfcmanager/ExampleUnitTest.java
@@ -1,8 +1,10 @@
 package com.example.nfcmanager;
 
 import org.junit.Test;
-
+import com.example.nfcmanager.dataController;
 import static org.junit.Assert.*;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +13,14 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void stringByteTest() {
+        dataController a = new dataController();
+        String b = "가나다abcd123";
+        byte[] c = a.stringToByte(b);
+        String d = a.byteToString(c);
+
+        assertEquals(d,"가나다abcd123");
+
     }
+
 }
