19d6b7ffeadbe084aed32b76f90f2b09be461fca
a8540a <a8540a@kyonggi.ac.kr>
2022-05-18 02:54:28 +0900

    rwactivity

---
+++ app/src/main/java/com/example/nfcmanager/rwActivity.java
@@ -0,0 +1,14 @@
+package com.example.nfcmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rwActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rw);
+    }
+}
